ce470f26350c67a6ddbb334c82f8fe71924a0d73
Dieter <dieter.ketels@abrumet.be>
2021-01-22 13:21:57 +0100

    First try: exceptions
    Should run but not working (maybe retry from scratch with HAPI FHIR R4)

--- src/main/java/com/example/fhirExample/FhirApplicationExampleApplication.java
+++ src/main/java/com/example/fhirExample/FhirApplicationExampleApplication.java
@@ -2,6 +2,8 @@ package com.example.fhirExample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FhirApplicationExampleApplication {
@@ -10,4 +12,10 @@ public class FhirApplicationExampleApplication {
 		SpringApplication.run(FhirApplicationExampleApplication.class, args);
 	}
 
+	@Bean
+	public ServletRegistrationBean ServletRegistrationBean() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(new SimpleRestfulServer(), "/*");
+		registration.setName("FhirServlet");
+		return registration;
+	}
 }
